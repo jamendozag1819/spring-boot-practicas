89e26fdc28f7965feedc514eaef411b3213f2aaf
Javier Mendoza <jamendozag@minsait.com>
2023-01-21 01:54:48 -0600

    creacion del proyecto

---
+++ spring-boot-web/src/test/java/com/minsait/springboot/web/app/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.springboot.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
